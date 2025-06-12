927954edaff25b1b72313bff7c82b0f34a3d07e8
fakharabbasbhatti <fakharabbasbhatti333@gmail.com>
2025-06-12 04:44:29 -0700

    fix: corrected import path case for Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import AboutMe from './components/AboutMe'
 import MySkills from './components/MySkills'
